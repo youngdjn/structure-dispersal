74c378521181bc74dc1cf824665f11c4fb4897fb
Derek <derek.j.n.young@gmail.com>
2021-04-23 15:50:31 -0700

    Delta on Vulcan with terra

--- scripts/tree-detection/dsm_to_chm.R
+++ scripts/tree-detection/dsm_to_chm.R
@@ -1,46 +1,50 @@
 ## Function to compare a drone map with a ground map and, ultimately, compare all drone maps in the directory with the ground map
-  
-  library(tidyverse)
-  library(sf)
-  library(raster)
-  library(here)
-  
-  
-  # The root of the data directory
-  data_dir = readLines(here("data_dir.txt"), n=1)
-  
-  source(here("scripts/convenience_functions.R"))
-  
-  #### Inputs ####
-  
-  # Project area boundary
-  focal_area = st_read(data("scouting/boundaries/chips_focal.geojson")) #%>% st_transform(32610)
-  
-  # DTM
-  dtm = raster(data("scouting/dtms/ChipsA_20200523T0842_dtm.tif"))
-  
-  # DSM file
-  dsm_file = data("scouting/metashape_outputs/ChipsA_20200523T0842_dsm.tif")
-  dsm = raster(dsm_file)
-  
-  # crop and mask DSM to project roi
-  dsm = crop(dsm, focal_area %>% st_transform(crs(dsm)))
-  dsm = mask(dsm,focal_area %>% st_transform(crs(dsm)))
-  
-  dtm = crop(dtm, focal_area %>% st_transform(crs(dtm)))
-  dtm = mask(dtm,focal_area %>% st_transform(crs(dtm)))
-  
-  # interpolate the the dtm to the res, extent, etc of the DSM
-  dtm_interp = resample(dtm %>% projectRaster(crs=crs(dsm)),dsm)
-  
-  
-  #### Calculate canopy height model ####
-  #### and save to tif
-  
-  # calculate canopy height model
-  chm = dsm - dtm_interp
+
+library(tidyverse)
+library(sf)
+library(terra)
+library(here)
+
+
+# The root of the data directory
+data_dir = readLines(here("data_dir.txt"), n=1)
+
+source(here("scripts/convenience_functions.R"))
+
+#### Inputs ####
+
+# Project area boundary
+focal_area = st_read(data("drone/boundaries/delta-boundary-from-photos.gpkg")) #%>% st_transform(32610)
+
+# DTM
+dtm = rast(data("metashape-products/delta_meta033_20210415T0728_dtm.tif"))
+
+# DSM file
+dsm_file = data("drone/metashape-products/delta_meta033_20210415T0728_dsm.tif")
+dsm = rast(dsm_file)
+
+# crop and mask DSM to project roi
+dsm = crop(dsm, focal_area %>% st_transform(crs(dsm)))
+dsm = mask(dsm,focal_area %>% st_transform(crs(dsm)))
+
+dtm = crop(dtm, focal_area %>% st_transform(crs(dtm)))
+dtm = mask(dtm,focal_area %>% st_transform(crs(dtm)))
+
+# interpolate the the dtm to the res, extent, etc of the DSM
+dtm_interp = resample(dtm %>% projectRaster(crs=crs(dsm)),dsm)
+
+
+#### Calculate canopy height model ####
+#### and save to tif
+
+# calculate canopy height model
+chm = dsm - dtm_interp
+
+# downscale to 0.12 m
+chm = project(chm,res=0.12, crs = "+proj=utm +zone=10 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs", method="bilinear")
+
 
 # create dir if doesn't exist, then write
-writeRaster(chm,data("scouting/chms/ChipsA_chm.tif"), overwrite=TRUE) # naming it metashape because it's just based on metashape dsm (and usgs dtm) -- to distinguish from one generated from point cloud
+writeRaster(chm,data("drone/processed-products/delta_meta033_20210415T0728_dtm.tif"), overwrite=TRUE) # naming it metashape because it's just based on metashape dsm (and usgs dtm) -- to distinguish from one generated from point cloud
 
 gc()
