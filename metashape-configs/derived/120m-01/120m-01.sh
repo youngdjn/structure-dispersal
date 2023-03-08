96346eb5f5e8476a68a3ca35062ab03a97c2b45b
Derek Young <djyoung@ucdavis.edu>
2023-03-08 04:35:01 +0000

    Create metashape configs

---
+++ metashape-configs/derived/120m-01/120m-01.sh
@@ -0,0 +1,7 @@
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/structure-dispersal/metashape-configs/derived/120m-01/ChipsB-120m.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/structure-dispersal/metashape-configs/derived/120m-01/Crater-120m.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/structure-dispersal/metashape-configs/derived/120m-01/DeltaA-120m.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/structure-dispersal/metashape-configs/derived/120m-01/DeltaB-120m.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/structure-dispersal/metashape-configs/derived/120m-01/EilerA-120m.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/structure-dispersal/metashape-configs/derived/120m-01/Lassic-120m.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/structure-dispersal/metashape-configs/derived/120m-01/ValleyA-120m.yml
