b0e805ca5e1293264b7af9fd8265276270279ee2
youngdjn <youngdjn>
2023-10-03 04:00:53 +0000

    Complete tree shifting

--- scripts/plot-data-carpentry/align-stemmap-plot-data.R
+++ scripts/plot-data-carpentry/align-stemmap-plot-data.R
@@ -7,11 +7,13 @@
 library(tidyverse)
 library(sf)
 
-unshifted = st_read("/ofo-share/str-disp_data-partial/surveys/main/processed/stems_v2.gpkg") |>
+unshifted_orig = st_read("/ofo-share/str-disp_data-partial/surveys/main/processed/stems_v2.gpkg") |>
     st_transform(3310)
-shifted = st_read("/ofo-share/str-disp_data-partial/surveys/main/processed/stems_v2_manualyShiftedSubset.gpkg") |>
+shifted_orig = st_read("/ofo-share/str-disp_data-partial/surveys/main/processed/stems_v2_manualyShiftedSubset.gpkg") |>
     st_transform(3310)
 
+unshifted = unshifted_orig
+shifted = shifted_orig
 
 # Test if a tree has been shifted: does it have the exact same coordinates in both datasets?
 
@@ -49,3 +51,41 @@ shifts = trees |>
 trees = trees |>
     left_join(shifts) |>
     mutate(x_shift = ifelse(is.na(x_shift), 0, x_shift), y_shift = ifelse(is.na(y_shift), 0, y_shift))
+
+# apply the shifts
+trees = trees |>
+    mutate(x_autoshifted = x_unshifted + x_shift, y_autoshifted = y_unshifted + y_shift)
+
+# pull in all original attributes
+trees = trees |>
+    select(fire, stem_map_id, center_id, tree_id, x_autoshifted, y_autoshifted) |>
+    left_join(unshifted_orig |> st_drop_geometry())
+
+# convert to spatial (sf)
+trees = trees |>
+    st_as_sf(coords = c("x_autoshifted", "y_autoshifted"), crs = 3310) |>
+    st_transform(4326) |>
+    select(fire, stem_map_id, center_id, tree_id, date, species, ht_lowest_ndl, ht_top, pct_prefire_green, pct_current_green, distance, azimuth, notes)
+
+# get lat/lon and convert back to tabular
+coords = st_coordinates(trees)
+
+trees = trees |>
+    mutate(lon = coords[, 1], lat = coords[, 2])
+
+# delete delta fire trees manually measured from center c16a in stem map 1
+trees = trees[(!((trees$fire == "Delta" & trees$stem_map_id == 1 & ((trees$center_id == "c16a" & !is.na(trees$center_id)))))), ]
+
+## Assign a stem map name that cobines fire and stem_map_id (but also removes the number suffix of the Delta_3 and Lassic_3 fires)
+trees[trees$fire == "Delta_3", "fire"] = "Delta"
+trees[trees$fire == "Lassic_3", "stem_map_id"] = 2 # Lassic_3 is actually a subset of Lassic 2 but with a different base loc so it needed to be shifted separately, but then merged with Lassic 2
+trees[trees$fire == "Lassic_3", "fire"] = "Lassic"
+
+trees$stem_map_name = paste(trees$fire, trees$stem_map_id, sep = "_")
+trees = trees |>
+    select(-fire, -stem_map_id) |>
+    select(stem_map_name, everything())
+
+# write to file
+write_csv(trees |> st_drop_geometry(), "/ofo-share/str-disp_data-partial/surveys/main/processed/stems_v2_aligned.csv")
+#st_write(trees, "/ofo-share/str-disp_data-partial/surveys/main/processed/stems_v2_aligned.gpkg", delete_dsn = TRUE)
