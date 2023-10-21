39cc48f8fcda776605a37d48592b681461893611
youngdjn <youndjn>
2023-10-21 02:19:02 +0000

    Framework: assigning field species to drone trees

---
+++ scripts/tree-species-id/assign-field-species-to-drone-trees.R
@@ -0,0 +1,15 @@
+
+
+# Load field trees
+# Load field perims
+
+# Load drone trees
+
+# Start with Chips
+
+# Filter both tree sets to comparable heights (field tree height can be < 10)...need to think if a mod to code is necessary given that field goes down to 10...what if drone tree is 10 and field was measured as 9.9 and excluded?
+
+# Make matches by calling command line arg
+
+# Assign species to drone trees based on the matched field tree
+# Export as crown polygon
