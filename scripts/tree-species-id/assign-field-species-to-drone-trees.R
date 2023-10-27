4f01b02d6da8c347d2fc7892bdb458eb48fddd0a
youngdjn <youndjn>
2023-10-27 04:19:54 +0000

    Add valley, chm height, clean up

--- scripts/tree-species-id/assign-field-species-to-drone-trees.R
+++ scripts/tree-species-id/assign-field-species-to-drone-trees.R
@@ -20,18 +20,15 @@ perims_field = st_read("/ofo-share/str-disp_drone-data-partial/cross-site/field-
 ## Get the field tree dataset into the expected format (column names, etc)
 trees_field$Height = trees_field$ht_top
 
-# if there's "snag" in the species, make the spacies "SNAG" and make percent green 0
+# If there's "snag" in the species, make the spacies "SNAG" and make percent green 0
 trees_field = trees_field |>
   mutate(species = toupper(species)) |>
   mutate(species = ifelse(str_detect(species, regex("snag", ignore_case = TRUE)), "SNAG", species),
         pct_current_green = ifelse(str_detect(species, regex("snag", ignore_case = TRUE)), 0, pct_current_green))
 
-
-
-
 # Make a data frame of all the stem maps we want, so we can loop through it
-stemmaps = data.frame(stem_map_name = c("Chips_1", "Chips_1_ABCO", "Chips_2", "Delta_1", "Delta_2", "Delta_3"),
-                      fire_name =     c("chips",   "chips",        "chips",   "delta",   "delta",    "delta"))
+stemmaps = data.frame(stem_map_name = c("Chips_1", "Chips_1_ABCO", "Chips_2", "Delta_1", "Delta_2", "Delta_3", "Valley_1"),
+                      fire_name =     c("chips",   "chips",        "chips",   "delta",   "delta",    "delta", "valley"))
 
 crowns_drone_w_field_data = data.frame()
 
@@ -75,7 +72,6 @@ for(i in 1:nrow(stemmaps)) {
 
   crowns_drone_foc = crowns_drone[crowns_drone_foc_idxs, ]
 
-
   # Run matching and filter to only matched trees
   matches = match_trees_singlestratum(trees_field_foc,
                                       trees_drone_foc,
@@ -85,11 +81,6 @@ for(i in 1:nrow(stemmaps)) {
 
   matches = matches |>
     filter(!is.na(final_predicted_tree_match_id))
-
-  ## Assign species to drone trees based on the matched field tree
-  # # Make two aligned data frames of the matching trees (field and drone; only those that match)
-  # trees_field_foc_match = trees_field_foc[match(matches$observed_tree_id, trees_field_foc$observed_tree_id), ]
-  # trees_drone_foc_match = trees_drone_foc[match(matches$final_predicted_tree_match_id, trees_drone_foc$predicted_tree_id),]
 
   ## Take the crown polygons and look up the species of the matched field tree
   # First get only the columns we need from the field tree data
@@ -101,13 +92,16 @@ for(i in 1:nrow(stemmaps)) {
            percent_green_observed = pct_current_green,
            stem_map_name,
            predicted_tree_id = final_predicted_tree_match_id) |>
-    mutate(live = as.numeric(percent_green_observed) > 0,
-           percent_green_observed = as.numeric(percent_green_observed))
+    mutate(live_observed = as.numeric(percent_green_observed) > 0,
+           percent_green_observed = as.numeric(percent_green_observed),
+           fire = fire_name_foc)
 
-  #  Join the field tree data to the drone crown polygons
+  #  Join the field tree data to the drone crown polygons, also pull in the photogrammetry tree height (from the treetop points)
   crowns_drone_foc_w_field_data = crowns_drone_foc |>
-    inner_join(trees_field_foc_simp, by = "predicted_tree_id")
-  
+    inner_join(trees_field_foc_simp, by = "predicted_tree_id") |>
+    left_join(trees_drone_foc |> st_drop_geometry(), by = join_by(predicted_tree_id, stem_map_name)) |>
+    rename(height_chm = height)
+
   # Bind onto running data frame
   crowns_drone_w_field_data = rbind(crowns_drone_w_field_data, crowns_drone_foc_w_field_data)
 
@@ -120,48 +114,20 @@ st_write(crowns_drone_w_field_data, "/ofo-share/scratch-derek/crowns_drone_w_fie
 
 
 
+# ## Vis: Make lines between matched trees, as well as drone and field reference trees
+# # Make two aligned data frames of the matching trees (field and drone; only those that match)
+# trees_field_foc_match = trees_field_foc[match(matches$observed_tree_id, trees_field_foc$observed_tree_id), ]
+# trees_drone_foc_match = trees_drone_foc[match(matches$final_predicted_tree_match_id, trees_drone_foc$predicted_tree_id),]
+# # Make lines pairing matches for vis
+# lines = st_sfc(mapply(function(a,b){st_cast(st_union(a,b),"LINESTRING")}, st_geometry(trees_field_foc_match), st_geometry(trees_drone_foc_match), SIMPLIFY=FALSE))
+# lines = st_sf(lines)
+# lines$observed_tree_id = trees_field_foc_match$observed_tree_id
+# lines$predicted_tree_id = trees_drone_foc_match$predicted_tree_id
 
+# st_crs(lines) = st_crs(trees_field_foc_match)
 
+# st_write(lines, "/ofo-share/scratch-derek/pairing-lines_custom.gpkg", delete_dsn = TRUE)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## Vis: Make lines between matched trees, as well as drone and field reference trees
-# Make two aligned data frames of the matching trees (field and drone; only those that match)
-trees_field_foc_match = trees_field_foc[match(matches$observed_tree_id, trees_field_foc$observed_tree_id), ]
-trees_drone_foc_match = trees_drone_foc[match(matches$final_predicted_tree_match_id, trees_drone_foc$predicted_tree_id),]
-# Make lines pairing matches for vis
-lines = st_sfc(mapply(function(a,b){st_cast(st_union(a,b),"LINESTRING")}, st_geometry(trees_field_foc_match), st_geometry(trees_drone_foc_match), SIMPLIFY=FALSE))
-lines = st_sf(lines)
-lines$observed_tree_id = trees_field_foc_match$observed_tree_id
-lines$predicted_tree_id = trees_drone_foc_match$predicted_tree_id
-
-
-
-
-
-st_crs(lines) = st_crs(trees_field_foc_match)
-
-st_write(lines, "/ofo-share/scratch-derek/pairing-lines_custom.gpkg", delete_dsn = TRUE)
-
-# Also write out drone and field ref trees for vis
-st_write(trees_field_foc, "/ofo-share/scratch-derek/trees_field_v2.gpkg", delete_dsn = TRUE)
-st_write(trees_drone, "/ofo-share/scratch-derek/trees_drone_v2.gpkg", delete_dsn = TRUE)
-
-# Assign species to drone trees based on the matched field tree
-# Export as crown polygon
-
+# # Also write out drone and field ref trees for vis
+# st_write(trees_field_foc, "/ofo-share/scratch-derek/trees_field_v2.gpkg", delete_dsn = TRUE)
+# st_write(trees_drone, "/ofo-share/scratch-derek/trees_drone_v2.gpkg", delete_dsn = TRUE)
