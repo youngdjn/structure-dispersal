c7b765cffcf6ff30804433454dbfd4406b9d138a
Andrew Latimer <amlatimer@ucdavis.edu>
2024-08-12 14:12:46 -0700

    change data for model run

--- scripts/dispersal-modeling/02_fit-dispersal-kernel-stan.R
+++ scripts/dispersal-modeling/02_fit-dispersal-kernel-stan.R
@@ -5,6 +5,7 @@
 
 library(here)
 
+
 #data_dir = readLines(here("data_dir.txt"), n = 1)
 # for Andrew's local work
 data_dir = "/Users/latimer/Library/CloudStorage/Box-Box/str-disp_data"
@@ -16,11 +17,11 @@ source(here("scripts/dispersal-modeling/02_fit-dispersal-kernel-stan_functions.R
 # Fit a model for a specific site, disp function, error model, and stan parameters. See
 # 02_fit-dispersal-kernel_functions.R for parameter definitions.
 m = fit_stan_model(
-  dataset_name = "delta-PINES", # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
+  dataset_name = "delta-ALL", # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
   disp_mod = "2Dt", # 2Dt or exppow
   err_mod = "pois", # pois only currently
   n_warmup = 500, # stan warmup iter
-  n_iter = 2500, # stan iter, includes warmup
+  n_iter = 1500, # stan iter, includes warmup
   n_chains = 4, # stan n chains
   n_cores = 4 # stan n cores
 )
@@ -69,7 +70,7 @@ m = fit_stan_model(
 #### Fit models with specified dispersal model parameterizion, in addition to dispersal kernel, error model, and species and site #### 
 
 # Model with multiplier and exponent 
-m2 = fit_stan_model_fecund(dataset_name = "delta-ALL",   # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
+m2 = fit_stan_model_fecund(dataset_name = "delta-PINES",   # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
                            disp_mod = "2Dt",                         # 2Dt or exppow
                            err_mod = "pois",                         # pois only currently
                            fecund_mod = "multiplier_exponent_noheight",                # multiplier, multiplier_exponent, multiplier_intercept, multiplier_exponent_intercept
