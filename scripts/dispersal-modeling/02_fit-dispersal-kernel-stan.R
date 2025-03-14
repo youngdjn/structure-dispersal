9af0d807f062c71528b7724338192a5f771f2e50
Andrew Latimer <amlatimer@ucdavis.edu>
2025-03-13 19:23:49 -0700

    cleaning this up so it works ok with contrasting fecundity models.

--- scripts/dispersal-modeling/02_fit-dispersal-kernel-stan.R
+++ scripts/dispersal-modeling/02_fit-dispersal-kernel-stan.R
@@ -6,9 +6,9 @@
 library(here)
 
 
-data_dir = readLines(here("data_dir.txt"), n = 1)
+#data_dir = readLines(here("data_dir.txt"), n = 1)
 # for Andrew's local work
-#data_dir = "/Users/latimer/Library/CloudStorage/Box-Box/str-disp_data"
+data_dir = "/Users/latimer/Library/CloudStorage/Box-Box/str-disp_data"
 
 
 # Main functions for the tasks in this script.
@@ -16,16 +16,26 @@ source(here("scripts/dispersal-modeling/02_fit-dispersal-kernel-stan_functions.R
 
 # Fit a model for a specific site, disp function, error model, and stan parameters. See
 # 02_fit-dispersal-kernel_functions.R for parameter definitions.
-m = fit_stan_model(
-  dataset_name = "delta-ALL", # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
+m_multiplier = fit_stan_model_fecund(
+  dataset_name = "delta-PIPJ", # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
+  disp_mod = "2Dt", # 2Dt or exppow
+  err_mod = "pois", # pois only currently
+  fecund_mod = "multiplier",
+  n_warmup = 500, # stan warmup iter
+  n_iter = 1500, # stan iter, includes warmup
+  n_chains = 3, # stan n chains
+  n_cores = 3 # stan n cores
+)
+#### NOTE: Current run uses more informative priors on dispersal parameters and slighly more informative prior on fecundity model multiplier parameter b. 
+
+m_multiplier_exponent = fit_stan_model(
+  dataset_name = "delta-PIPJ", # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
   disp_mod = "2Dt", # 2Dt or exppow
   err_mod = "pois", # pois only currently
   n_warmup = 500, # stan warmup iter
-  n_iter = 1500, # stan iter, includes warmup
-  n_chains = 4, # stan n chains
-  n_cores = 4 # stan n cores
-)
-#### NOTE: Current run uses more informative priors on dispersal parameters and slighly more informative prior on fecundity model multiplier parameter b. 
+  n_iter = 2000, # stan iter, includes warmup
+  n_chains = 3, # stan n chains
+  n_cores = 3 # stan n cores
 
 m = fit_stan_model(
   dataset_name = "delta-FIRS", # which dataset to model (corresponding data files must be in datadir/prepped-for-stan/{dataset_name}), produced by 01_prep-data-for-model.R
