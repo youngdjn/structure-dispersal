d0e8680b436d33062ba6417001a4ca2cb85e78c3
Andrew Latimer <amlatimer@ucdavis.edu>
2025-03-19 17:45:06 -0700

    expanded explanation of prior selection, slightly changed prior settings for 2Dt model

--- scripts/dispersal-modeling/00_selecting-priors.R
+++ scripts/dispersal-modeling/00_selecting-priors.R
@@ -1,26 +1,20 @@
 # This is exploratory code used for selecting Stan priors
 
+#### Height Difference model ####
+
 scalar_fun = function(ht_diff, b1) {
-  
   scalar = exp(b1 * ht_diff)
-  
 }
 
-
 b0 = 0
-
 log_b1 = -5.298317  # log(0.005)
-
 b1 = exp(log_b1)
-
-
 
 field_ht_diff = seq(-300, 300, by = 10)
 
 scalars = scalar_fun(field_ht_diff, b1)
 
 plot(field_ht_diff, scalars)
-
 
 # 0.0001    0.005      0.010
 
@@ -30,16 +24,18 @@ log(0.01)
 
 # -5, 3
 
-# Checking priors on 2Dt model 
+#### Dispersal Models ####
+
+## 2Dt dispersal model ##
 
 calc_kern_2Dt_priors = function(a, k, r) {
   kern = k / (pi*a) * (1 + r^2/a)^(-1-k) # 2Dt
   kern
 }
 
-a = 30000 # a shouldn't go below about 1000 or above about 30000
-# could set prior on log(a) ~ normal(8.5, 0.5) to try and hold it in that range 
-k = 3
+a = 480000 # a shouldn't go below about 1000 or too much above about 100000
+# could set prior on log(a) ~ normal(9, 0.5) to try and hold it in that range 
+k = 0.1
 r = 0:500
 kern = calc_kern_2Dt_priors(a = a, k = k, r = r)
 kern_df_1 = data.frame(r = r, kern = kern, a = a, k = k)
@@ -50,22 +46,38 @@ ggplot(kern_df_1, aes(x=r, y=kern)) +
 kern_df_1[101,]
 kern_df_1$kern[10] * (exp(1.94)*20^1.2) * seedling_plot_area
 
-
-
-# priors are set on the log of a for some reason 
-log(30000) # alpha shouldn't go much above 10 
+# priors are set on the log of a 
+log(100000) # alpha shouldn't go much above 11 or 12 
 log(1000) # or much below 7 
+
+# Specify prior as: 
+# a ~ exp(alpha)
+# alpha ~ dnorm(9, sd = 0.5)
+
 
 # Prior on k is set through inv_log_k = inv_logit(inv_k) where inv_k ~ normal(0, 1) 
 library(boot)
-inv_k_real = -2
+inv_k_real = 0.55
 (1 + exp(-inv_k_real))/2
 1/ (2 * inv.logit(inv_k_real)) # ok this is the same! 
-# To keep k within about 0.5 and 2, inv_k should be between -1 and 2 
-# So inv_k ~ normal(0.5, 0.5) should work
+# At very small k, the dispersal kernel gets long, but this has minimal effect when a is large, it seems, so maybe no huge problem with allowing a k to get small. 
+# At k over about 5 to 10, the dispersal kernel gets very short / truncated. 
+# To keep k below about 5, inv_k should be above -2.5. 
+# In this prior specification, k can't get smaller than about 0.5 -- Q IS THIS A PROBLEM? MAYBE REPARAMETERIZE? CHECK MODEL. 
 
-# Prior on b is via log(b) or mu_beta 
-# Note smallest tree is 10m tall, so b should be set to give between say 5 and 50 seeds per meter of height? 
+# NOTE IN MODEL, k ends up pretty jammed up against the 0.5 limit while a wants to be big. Worth trying priors that let k get smaller than that? 
+
+# Specify prior on inv_k as 
+k = 1 / (2*inv.logit(inv_k_real))
+inv_k_real ~ dnorm(1, 1)
+
+## Exponential dispersal model ###
+
+
+#### Fecundity Models ####
+
+# Background info about fecundity vs size: 
+# Note smallest tree is 10m tall. 
 # Nina's data suggest 1300 (~500-3000) germinants per uninjured median-sized yellow pine 
 # For a 20m tree, that's 65 germinants per meter of height
 # For median height sugar pine, Nina's data shows more seed production: 3900 (~1000-10,000). 
@@ -73,10 +85,15 @@ inv_k_real = -2
 # For white fire, her data show much higher seed production: 13,800 germinants, with little variation by tree size 
 # For a 20m tree, that's 690 germinants per meter of height
 
-# Using that info to set priors for pines, we can try and constrain b to be between about 25 and 500 (germinants per meter of tree height)
+
+## Multiplier model -- seedlings = b * height_m ## 
+
+# Note prior is set on log(beta)
+
+# Using that info to set priors for pines, we can try and constrain b to be between about 25 and 300 (germinants per meter of tree height)
 log(25) # 3.2
-log(500) # 6.2
-# So mu_beta could be set at normal(4.7, 0.5) to keep b in that range
+log(300) # 5.7
+# So mu_beta could be set at normal(4.4, 0.5) to push b toward that range
 
 
 # That's for seeds. But our model is for seedlings. Marchand et al. used a much lower prior for seedling counts: N(-2, 3). But note they are predicting seedlings per unit basal area of tree. Whereas we are using heights... Which should be related to DBH via roughly a 2/3 exponent. 
@@ -84,6 +101,7 @@ f <- function(x) {return(1.47*x^0.68)}
 plot(1:40, f(1:40)) # not too nonlinear, so we can roughly think of seedlings per meter of height as about half the number of seedlings per cm of DBH 
 # We might want to adjust Marchand et al's prior downward by 50% 
 #normal(-2, 3) was a pretty wide prior for seedlings per unit basal area of tree.
+curve(dnorm(x, mean = -2.5, sd = 4), from = -10, to = 10)
 # we could aim for something that gives about half as many seedlings and varies a bit less. 
 exp(-11)
 exp(-2)
@@ -92,3 +110,5 @@ exp(7)
 exp(-2.5)
 exp(-10)
 exp(5)
+
+## Exponential model -- seedlings = b * height_m ^ zeta ## 
