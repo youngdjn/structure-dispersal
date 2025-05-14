155535b6b460ceb3a0198ffd5ee7dd2721232646
Andrew Latimer <amlatimer@ucdavis.edu>
2025-05-13 17:58:18 -0700

    Got this to work -- but won't converge so far

--- scripts/dispersal-modeling/test_model_fitting_with_optim.R
+++ scripts/dispersal-modeling/test_model_fitting_with_optim.R
@@ -11,16 +11,17 @@ q_fun <- function(b, n_overstory_trees, overstory_tree_size) {
 
 # Dispersal kernel function (vectorized calculation for a single plot)
 disp_prob <- function(k, a, n_overstory_trees, dist_vector) { 
-  kernel_prob = exp(-pow(dist_vector/a, k))*k / (2*pi()*square(a)*tgamma(2/k)) # apply exponential power kernel for all trees associated with a given plot
+  kernel_prob = exp(-(dist_vector/a)^k)*k / (2 * pi * a^2 * gamma(2/k)) # apply exponential power kernel for all trees associated with a given plot
   return(kernel_prob)
 }
   
 # Calculate expected/fitted seedlings per plot 
 calc_mu <- function(k, a, b, n_overstory_trees, dist_vector, overstory_tree_size, pos, seedling_plot_area) {
   n_seedling_plots = length(n_overstory_trees) 
+  mu = vector(mode = "numeric", length = n_seedling_plots)
   for(i in 1:n_seedling_plots){
       segment_start = pos[i]
-      segment_end = pos[i] + n_overstory_trees[i]
+      segment_end = pos[i] + n_overstory_trees[i] - 1
       mu[i] = sum( disp_prob(k, a, n_overstory_trees[i], 
                 dist_vector[segment_start:segment_end]) * 
                 q_fun(b, n_overstory_trees[i], 
@@ -31,9 +32,13 @@ calc_mu <- function(k, a, b, n_overstory_trees, dist_vector, overstory_tree_size
 }
 
 # Function to get the negative log likelihood for a set of parameter values
-calc_negloglik <- function(k, a, b, n_overstory_trees, dist_vector, overstory_tree_size, pos, seedling_counts, seedling_plot_area) {
-  mu = calc_mu(k, a, b, n_overstoy_trees, dist_vector, overstory_tree_size, pos) 
+calc_negloglik <- function(pars, n_overstory_trees, dist_vector, overstory_tree_size, pos, seedling_counts, seedling_plot_area) {
+  b = pars[1]
+  k = pars[2]
+  a = pars[3]
+  mu = calc_mu(k, a, b, n_overstory_trees, dist_vector, overstory_tree_size, pos, seedling_plot_area) 
   negloglik = -sum(dpois(x = seedling_counts, lambda = mu, log=TRUE))
+  return(negloglik)
 }
 
 
@@ -79,7 +84,7 @@ fit_model_optim <- function(startpars, n_overstory_trees = n_overstory_trees, di
                   k=fit$par[2],
                   a=fit$par[3])
 
-  fv <- calc_mu(b = fit$par[1], k = fit$par[2], a = fit$par[3], n_overstory_trees = n_overstory_trees, dist_vector = dist_vector, overstory_tree_size = overstory_tree_size, pos = pos, seedling_counts = seedling_counts, seedling_plot_area = seedling_plot_area)
+  fv <- calc_mu(b = fit$par[1], k = fit$par[2], a = fit$par[3], n_overstory_trees = n_overstory_trees, dist_vector = dist_vector, overstory_tree_size = overstory_tree_size, pos = pos, seedling_plot_area = seedling_plot_area)
   
   res<-list(estimates=estimates,negloglik=fit$value,fitted.values=fv,call=cl,
             counts=fit$counts,convergence=fit$convergence,message=fit$message)
@@ -87,4 +92,65 @@ fit_model_optim <- function(startpars, n_overstory_trees = n_overstory_trees, di
   return(res)
 }
 
+get_disp_data <- function(dataset_name) # corresponding data files in datadir/prepped-for-stan/{dataset_name}
+  { 
+  
+  # -- Load prepped dataset (corresponding data files in datadir/prepped-for-stan/{dataset_name})
+  prepped_data_dir = file.path(data_dir, "prepped-for-stan", dataset_name)
+  
+  seedling_plot_area = read_file(file.path(prepped_data_dir, "plot-area.txt")) |> 
+    as.numeric()
+  dist_vector = read_lines(file.path(prepped_data_dir, "dist-vector.txt")) |>
+    as.numeric() |>
+    as.vector()
+  overstory_treesize_vector = read_lines(file.path(prepped_data_dir,
+    "overstory-treesize-vector.txt")) |>
+    as.numeric() |>
+    as.vector()
+  seedling_counts = read_lines(file.path(prepped_data_dir, "seedling-counts.txt")) |>
+    as.numeric() |>
+    as.vector()
+  n_overstory_trees = read_lines(file.path(prepped_data_dir, "n-overstory-trees.txt")) |>
+    as.numeric() |>
+    as.vector()
+  pos = read_lines(file.path(prepped_data_dir, "pos.txt")) |>
+    as.numeric() |>
+    as.vector()
+  
+  ## Compile data and priors into list for Stan
+  
+  data_list <- lst(
+    seedling_plot_area,
+    n_overstory_trees,
+    n_seedling_plots = length(seedling_counts),
+    overstory_tree_size = overstory_treesize_vector,
+    seedling_counts,
+    dist_vector,
+    obs = length(dist_vector),
+    pos
+  )
+
+  # Check for missing data
+  if (any(is.na(unlist(data_list)))) stop("Missing values in data.")
+  
+  return(data_list)
+}
+
 #### Test it out 
+
+disp_data = get_disp_data(dataset_name = "delta-ALL")
+
+calc_negloglik(pars = pars.init, 
+    n_overstory_trees = disp_data$n_overstory_trees, 
+    dist_vector = disp_data$dist_vector, 
+    overstory_tree_size = disp_data$overstory_tree_size, 
+    pos = disp_data$pos, 
+    seedling_counts = disp_data$seedling_counts, 
+    seedling_plot_area = disp_data$seedling_plot_area)
+
+startpars = list(b = 1, k = 1.5, a = 60)
+
+fit_model_optim(startpars, n_overstory_trees = n_overstory_trees, dist_vector = dist_vector, overstory_tree_size = overstory_tree_size, pos = pos, seedling_counts = seedling_counts, seedling_plot_area = seedling_plot_area)
+
+# Wants to run away to very high values of fecundity and low values of dispersal. 
+
