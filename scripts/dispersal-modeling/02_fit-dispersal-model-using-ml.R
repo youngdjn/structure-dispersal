e54891e81fcf2f831fd081568bf1668b95257784
Andrew Latimer <amlatimer@ucdavis.edu>
2025-07-19 14:26:24 -0700

    clean up the beginning setup stuff

--- scripts/dispersal-modeling/02_fit-dispersal-model-using-ml.R
+++ scripts/dispersal-modeling/02_fit-dispersal-model-using-ml.R
@@ -6,48 +6,69 @@
 library(here)
 library(tidyverse)
 
+source("./scripts/dispersal-modeling/01_load-data-for-modeling.R")
+source("./scripts/dispersal-modeling/02_fit-model-using-ml-functions.R")
+
 # Set data directory -- detect whether on Jetstream vs Andrew's machine and set accordingly
 if (grep("latimer", here()) == 1) data_dir = readLines(here("data_dir_andrew.txt"), n = 1) else data_dir = readLines(here("data_dir.txt"), n = 1)
 
-# Load functions 
-source("./scripts/dispersal-modeling/02_fit-model-using-ml-functions.R")
-
-
 # Load dataset from one fire 
-disp_data_dir = file.path(data_dir, "max500")
-disp_data = get_disp_data(dataset_name = "delta-FIRS", data_dir = data_dir)
 
-# Or instead load a simulated dataset 
-#seedling_plot_area = 201
-#n_seedling_plots = n_plots
+# NOTE FOR NOW PROCEED WITH JUST DELTA -- NEED TO STRAIGHTEN OUT DATA FILE STRUCTURE!!!! 
+#   ALL THESE FILE PATHS ARE PROVISIONAL JUST TO TEST FUNCTIONS 
 
+disp_data_dir = file.path(data_dir)
+disp_data = get_dispdata(data_dir = disp_data_dir, 
+                          #site_name = "delta", # e.g. "delta"
+                          focal_species = "ABCO", # 4-letter code
+                          overstory_tree_filepath = "predicted-treecrowns-w-predicted-species/delta.geojson",
+                              # relative to `datadir`
+                          seedling_plot_filepath = "regen-plots-standardized/delta.gpkg", 
+                              # relative to `datadir`
+                          target_crs = 3310, # target CRS (to project the raw data sources to)
+                          seedling_plot_area = 201, # area of the plot in sq m
+                          min_tree_height = 10, # ignore trees shorter than this (in meters)
+                          density_raster_resolution = 10, # grid cell size for calculating local tree density in meters
+                          tree_distance_cutoff = 500 # ignore trees farther than this from a plot (meters)
+                          
+)
 
+# Alternatively: simulate a dataset 
 
-#disp_data = lst(seedling_plot_area,
-#                      n_overstory_trees,
-#                     n_seedling_plots, 
-#                    overstory_tree_size,
-#                   seedling_counts,
-#                      dist_vector,
-#                      obs = length(dist_vector),
-#                      pos)
+# disp_data = simulate_dispdata(domain_size = 800, # length of one side of simulated square "domain" 
+#   center_buffer = 200, # distance from central area with trees to edge of "domain" 
+#   trees_per_hectare = 100, # density of trees to simulate
+#   n_plots = 200, # number of seedling plots to simulate
+#   a = 30,     # scale parameter for exppow dispersal kernel
+#   k = 0.5,    # shape parameter for exppow dispersal kernel
+#   b = 10,     # fecundity coefficient 
+#   seedling_plot_area = 201,  # size of each plot in m²
+#   elev_range = 100, # elevation range (m) for simulated DEM 
+#   min_tree_height = 10, # min tree height (m) for simulation 
+#   max_tree_height = 30, # max tree height (m) for simulation 
+#   density_raster_resolution = 15, # grid cell size for calculating local tree density 
+#   tree_distance_cutoff = 300 # ignore trees farther than this from a plot
+# )
 
-# check that the model converges from dispersed initial values 
+# Set some diffent initial parameter values to check convergence
 startpars1 = list(b = 10, k = 0.5, a = 10)
 startpars2 = list(b = 10, k = 0.5, a = 10, theta = 5)
 
-# Convergence is sensitive to starting values -- can converge to "reasonable" or extreme values for most data sets 
-# Using 500m distance seems slighly more stable (more informative)
 
+# settings: a named list containing the values for all the options 
+#    available for model fitting. These include: 
+#    - lik_distrib = the data distribution for the model (pois or negbin)
+#    - disp_kernel = which dispersal kernel to use (exppow, 2Dt, lognomal, wald)
+#    - fecundity_fn = which fecundity function to use (linear or exponential)
+#    - optimizer = which optimizer to tell optim to use (BFGS, Nelder-Mead, etc)
+settings_to_use = list(lik_distrib = "pois", 
+                       disp_kernel = "exppow", 
+                       fecundity_fn = "linear", 
+                       optimizer = "BFGS")
 
 # Check likelihood calculation
-calc_negloglik(pars = c(startpars1$b, startpars1$k, startpars1$a), 
-               n_overstory_trees = disp_data$n_overstory_trees, 
-               dist_vector = disp_data$dist_vector, 
-               overstory_tree_size = disp_data$overstory_tree_size, 
-               pos = disp_data$pos, 
-               seedling_counts = disp_data$seedling_counts, 
-               seedling_plot_area = disp_data$seedling_plot_area, lik_distrib = "pois")
+calculate_negloglik(pars = c(startpars1$b, startpars1$k, startpars1$a), 
+               disp_data = disp_data, settings = settings_to_use)
 # It's sensitive to extreme values of k and a (gives NLL = Inf)
 
 m1 = fit_model_optim(startpars1, 
@@ -141,6 +162,10 @@ ggplot(lik_surface_data, aes(x = b, y = k)) +
   labs(title = "Likelihood surface", x = "b", y = "k") +
   theme(legend.position = "bottom") +
   guides(fill = guide_colorbar(title = "Negative log likelihood"))
+
+
+# NOTE: Convergence is sensitive to starting values -- can converge to "reasonable" or extreme values for most data sets 
+# NOTE: Using 500m distance seems slighly more stable (more informative)
 
 
 ## Next steps: 
